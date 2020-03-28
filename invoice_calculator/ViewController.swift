a5fa498bdbe0926c7f50e8671cf27b8b13e069a3
monet <tautei.net@gmail.com>
2020-03-28 11:28:28 +0800

    Initial Commit

---
+++ invoice_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  invoice_calculator
+//
+//  Created by monet-macbook-pro on 2020/3/28.
+//  Copyright © 2020 monet-macbook-pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
